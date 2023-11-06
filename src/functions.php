3e0f38d4e81d6f6e66fd0ac75425c9a564dce0b5
Kenny1911 <o-muzyka@mail.ru>
2023-11-06 23:13:19 +0400

    feat: Create duck_type function

--- src/functions.php
+++ src/functions.php
@@ -1,3 +1,23 @@
 <?php
 
 declare(strict_types=1);
+
+namespace Kenny1911\DuckType;
+
+use Kenny1911\DuckType\CodeGen\ClassGenerator;
+use Kenny1911\DuckType\Wrapper\EvalWrapper;
+
+/**
+ * @template T of object
+ * @param class-string<T> $interface
+ * @param object $object
+ * @return T
+ */
+function duck_type(string $interface, object $object): object
+{
+    static $wrapper = new EvalWrapper(
+        new ClassGenerator('Impl', EvalWrapper::class.'\\__Eval__')
+    );
+
+    return $wrapper->wrap($interface, $object);
+}
